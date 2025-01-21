b18f3e43c49765f52742b0315625baa9fcf7d3a3
Gokulashik M <119474711+Gokulashik22014@users.noreply.github.com>
2025-01-21 22:14:45 +0530

    version0.0

--- src/components/CustomDiv.jsx
+++ src/components/CustomDiv.jsx
@@ -3,10 +3,11 @@ import { motion } from 'framer-motion';
 const CustomDiv = ({ children,className }) => {
     return (
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 2, ease:'easeInOut' }}
+        initial={{ opacity: 0,y:75 }}
+        animate={{ opacity: 1,y:0 }}
+        transition={{duration:0.5,delay:0.25}}
         className={`bg-transparent backdrop-blur-sm rounded-md px-4 py-4 shadow-sm shadow-white ${className}`}
+        
       >
         {children}
       </motion.div>
--- src/pages/FirstSection.jsx
+++ src/pages/FirstSection.jsx
@@ -4,22 +4,20 @@ import About from "../components/About/About";
 import CuteAnimal from "../components/ModelComponents/CuteAnimal";
 import CrystalComponent from "../components/ModelComponents/CrystalComponent";
 import CustomBubbleText from "../components/CustomBubbleText";
+import TitleAnimation from "../components/TitleAnimation";
+import SplitText from '../components/SplitText'
 const FirstSection = () => {
   return (
     <div className="w-full">
-      <CustomDiv>
-        <h1 className="text-5xl text-white text-center mb-4">
-          DesignTrix 2025
-        </h1>
-      </CustomDiv>
+      <SplitText text="DesignTrix 2025" className="text-5xl w-full h-full text-white-900" />
       <div className="w-full flex flex-row">
         <About />
         {/* 3D Carousal with Cute Animal */}
         <div className="relative w-1/2 flex items-center justify-center">
           <CuteAnimal />
-          {/* Crystals around the CuteAnimal */}
+          
           <div className="absolute w-full h-full flex items-center justify-center -z-[3]">
-            {/* Top Crystal */}
+            
             <CrystalComponent />
           </div>
         </div>
