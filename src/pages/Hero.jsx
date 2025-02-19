6968e8b49081f80a750a756f7d3d14075d396731
Balamurugan R <shakthibala1884@gmail.com>
2025-02-19 10:55:29 +0530

    Added loadingScreen

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import HeroComponent from '../components/Hero/HeroComponent'
 
 const Hero = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay (e.g., fetching data or waiting for assets to load)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Adjust time as needed
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <HeroComponent/>
+    <div>
+      
+        <HeroComponent />
+      
+    </div>
   )
 }
 
